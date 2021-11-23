ab91252231263ee6a9e3c9be3520ecaa30e30e91
mosdefinite <d.e.fairchild@gmail.com>
2021-11-23 11:11:42 -0500

    created pet and owner, and started medical records

---
+++ backend/Pet Health Tracker/Pet Health Tracker/Models/Owner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pet_Health_Tracker.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual List<Pet> Pets { get; set; }
+
+        public Owner()
+        {
+
+        }
+
+
+    }
+}
---
+++ backend/Pet Health Tracker/Pet Health Tracker/Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pet_Health_Tracker.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Gender { get; set; }
+        public string Photo { get; set; }
+        public int OwnerId { get; set; }
+        public virtual Owner Owner { get; set; }
+    }
+}
